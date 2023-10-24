8bff2ec631386e761cf2bcf8f8ef87d3376a6b7a
Louis <roachlouis@hotmail.com>
2023-10-24 16:44:07 +0100

    getting links to work, article card added

---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function ArticleCard() {
+  const { id } = useParams(); 
+  const [article, setArticle] = useState(null);
+
+  useEffect(() => {
+   
+    fetch(`https://louis-api.onrender.com/api/articles/${id}`)
+      .then((res) => res.json())
+      .then((data) => setArticle(data.article));
+  }, [id]);
+
+  if (!article) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      <p>Author: {article.author}</p>
+      <p>Topic: {article.topic}</p>
+      <p>Content: {article.content}</p>
+     
+    </div>
+  );
+}
+
+export default ArticleCard
